1f61a4f7fbe5479a6be1010f731fee5998f2d4d0
ieko city <icpiot100@gmail.com>
2026-07-16 23:53:51 +1000

    Bump Home Energy Manager panel version

--- examples/www/home-energy-manager-panel.js
+++ examples/www/home-energy-manager-panel.js
@@ -2,7 +2,7 @@ import "./home-energy-manager-policy-card.js?v=007";
 import "./home-energy-manager-report-card.js?v=301";
 import "./home-energy-manager-debug-card.js?v=034";
 
-const HOME_ENERGY_MANAGER_PANEL_BUILD = "028";
+const HOME_ENERGY_MANAGER_PANEL_BUILD = "030";
 const HOME_ENERGY_MANAGER_PANEL_THEME_KEY = "home-energy-manager.panel.theme";
 const HOME_ENERGY_MANAGER_PANEL_PAGE_KEY = "home-energy-manager.panel.page";
 const HOME_ENERGY_MANAGER_PANEL_DEBUG_KEY = "home-energy-manager.panel.debug";
